7f04c84c1b245eb9535a10e773c4f5f6f8c14098
GAMINGNOOBdev <maho.mobilevm@outlook.de>
2024-06-27 13:14:20 +0200

    Undo changes (got worse)

--- js/windowsize.js
+++
@@ -1,4 +0,0 @@
-var websiteWidth = 1920;
-var scale = screen.width / websiteWidth;
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=' + websiteWidth + ', initial-scale=' + scale + '');
